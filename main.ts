ab151eac0d6f0d461720d07b4bce877f00dcb8cd
michael-zenge <michael.zenge@gmail.com>
2020-11-02 11:15:38 +0100

    Added sprite images for left, right, up movement

--- main.ts
+++ main.ts
@@ -9,9 +9,21 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         Annabelle.vy = -200
     }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Annabelle.setImage(aPrincess[2])
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    Annabelle.setImage(aPrincess[0])
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    Annabelle.setImage(aPrincess[0])
+})
 scene.onOverlapTile(SpriteKind.Locked, sprites.dungeon.chestOpen, function (sprite, location) {
     sprite.say("I found a key!", 500)
     sprite.setKind(SpriteKind.Unlocked)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Annabelle.setImage(aPrincess[3])
 })
 scene.onOverlapTile(SpriteKind.Unlocked, myTiles.tile10, function (sprite, location) {
     sprite.say("(A) unlock.", 500)
@@ -22,10 +34,12 @@ scene.onOverlapTile(SpriteKind.Unlocked, myTiles.tile10, function (sprite, locat
 scene.onOverlapTile(SpriteKind.Locked, myTiles.tile10, function (sprite, location) {
     sprite.say("Locked!", 500)
 })
+scene.onOverlapTile(SpriteKind.Unlocked, myTiles.tile12, function (sprite, location) {
+    Annabelle.setImage(aPrincess[1])
+})
 let Annabelle: Sprite = null
-scene.setBackgroundColor(13)
-tiles.setTilemap(tilemap`level`)
-Annabelle = sprites.create(img`
+let aPrincess: Image[] = []
+aPrincess = [img`
     . . . . . . 5 . 5 . . . . . . . 
     . . . . . f 5 5 5 f f . . . . . 
     . . . . f 1 5 2 5 1 6 f . . . . 
@@ -42,7 +56,62 @@ Annabelle = sprites.create(img`
     . . . f 3 3 5 3 3 5 3 3 f . . . 
     . . . f f f f f f f f f f . . . 
     . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Locked)
+    `, img`
+    . . . . . . . 5 5 . . . . . . . 
+    . . . . . f 5 5 5 5 f . . . . . 
+    . . . . f 6 6 6 6 6 6 f . . . . 
+    . . . f 6 1 1 1 6 1 6 6 f . . . 
+    . . . f 6 6 6 6 6 6 6 6 f . . . 
+    . . . f 6 6 6 6 6 6 6 6 f . . . 
+    . . . f 6 6 6 6 6 6 6 6 f . . . 
+    . . f f 6 6 6 6 6 6 6 6 f f . . 
+    . f 6 6 6 f 6 6 6 6 f 6 6 6 f . 
+    . . f f f 3 f f f f 3 f f f . . 
+    . . . f d 5 3 3 3 3 5 d f . . . 
+    . . f d d f 3 3 3 3 f d d f . . 
+    . . . f f f 5 3 3 5 f f f . . . 
+    . . . . f 3 3 5 5 3 3 f . . . . 
+    . . . . f 3 3 3 3 3 3 f . . . . 
+    . . . . . f f f f f f . . . . . 
+    `, img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f . . . . . . 
+    . . . . f 6 2 5 5 6 f . . . . . 
+    . . . f 6 6 6 6 1 6 6 f . . . . 
+    . . . f 6 6 6 6 6 1 6 f . . . . 
+    . . . f d f d 6 6 6 1 f . . . . 
+    . . . f d f d 6 6 6 6 f f . . . 
+    . . . f d 3 d d 6 6 6 f 6 f . . 
+    . . . . f d d d f f 6 f f . . . 
+    . . . . . f f 5 3 f 6 6 6 f . . 
+    . . . . f 5 3 3 f f f f f . . . 
+    . . . . f 3 3 f d f . . . . . . 
+    . . . . . f 3 f d f . . . . . . 
+    . . . . f 3 5 3 f d f . . . . . 
+    . . . . f f 3 3 f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `, img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f . . . . . . 
+    . . . . f 6 2 5 5 6 f . . . . . 
+    . . . f 6 6 6 6 1 6 6 f . . . . 
+    . . . f 6 6 6 6 6 1 6 f . . . . 
+    . . . f d f d 6 6 6 1 f . . . . 
+    . . . f d f d 6 6 6 6 f f . . . 
+    . . . f d 3 d d 6 6 6 f 6 f . . 
+    . . . . f d d d f f 6 f f . . . 
+    . . . . . f f 5 3 f 6 6 6 f . . 
+    . . . . f 5 3 3 f f f f f . . . 
+    . . . . f 3 3 f d f . . . . . . 
+    . . . . . f 3 f d f . . . . . . 
+    . . . . f 3 5 3 f d f . . . . . 
+    . . . . f f 3 3 f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `]
+aPrincess[3].flipX()
+scene.setBackgroundColor(13)
+tiles.setTilemap(tilemap`level`)
+Annabelle = sprites.create(aPrincess[0], SpriteKind.Locked)
 tiles.placeOnTile(Annabelle, tiles.getTileLocation(9, 5))
 controller.moveSprite(Annabelle, 100, 0)
 scene.cameraFollowSprite(Annabelle)
